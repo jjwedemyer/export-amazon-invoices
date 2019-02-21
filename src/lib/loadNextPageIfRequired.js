b6554976ff6359d9c4b8f7a44a68036e9c314cd8
Jakob Wedemeyer <jakob@wedemeyer.me>
2019-02-21 19:03:33 +0100

    removing logs

--- src/lib/loadNextPageIfRequired.js
+++ src/lib/loadNextPageIfRequired.js
@@ -1,23 +1,17 @@
 import listOrders from './listOrders';
-import {logStatus,log} from './log';
+import {logStatus, log, logError} from './log';
 
 import {resultsPerPage} from './constants';
 
 const loadNextPageIfRequired = async (page, index, numberOfOrders, year) => {
-	log(`index ${index}, numberOfOrders ${numberOfOrders}, year: ${year}`)
   const resultsPage = Math.ceil(index / resultsPerPage);
 
   const isFirstResultOnPage = index % resultsPerPage === 1;
   if (isFirstResultOnPage) {
     logStatus(`Processing results page ${resultsPage} of ${Math.ceil(numberOfOrders / 10)}`);
 
-    let offset = 0;
+    const offset = (resultsPage - 1) * resultsPerPage;
 
-    if ( numberOfOrders > 10 ) {
-       offset = resultsPage * resultsPerPage;
-    }
-
-    log(`offset ${offset}`);
     await page.goto(listOrders(year, offset), {waitUntil: 'load'});
   }
 };
