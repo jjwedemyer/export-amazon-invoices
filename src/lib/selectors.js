5449acb4382111e9154b2ce69388b2b65f368a1b
Jakob Wedemeyer <jakob@wedemeyer.me>
2019-02-21 19:03:55 +0100

    updating selectors

--- src/lib/selectors.js
+++ src/lib/selectors.js
@@ -1,12 +1,12 @@
 const selectors = {
   list: {
     numOrders: '#controlsContainer .num-orders-for-orders-by-date .num-orders',
-    order: '#ordersContainer .order',
+    order: '#ordersContainer div.order',
     orderDetails: {
       article: '.shipment .a-col-left .a-col-right .a-row:first-of-type .a-link-normal',
       date: '.order-info > div > div > div > div.a-fixed-right-grid-col.a-col-left > div > div.a-column.a-span4 > div.a-row.a-size-base > span',
       id: '.order-info .actions .a-size-mini .a-color-secondary+.value',
-      total: '.order-info .a-fixed-right-grid-col .a-span-4 .a-size-base .value',
+      total: '.order-info .a-fixed-right-grid-col .a-span4 .a-size-base .value',
       url: '.order-info .actions .a-row:nth-of-type(2) > .a-unordered-list > .a-link-normal:first-of-type',
     },
     page: '#yourOrders',
